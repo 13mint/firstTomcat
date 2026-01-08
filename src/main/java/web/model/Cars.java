48ffdfcbadec9640b15901533c2b6b072c516f58
13mint <13mint07pineapple13@gmail.com>
2026-01-08 14:03:14 +0900

    update dependencies

--- src/main/java/web/model/Cars.java
+++ src/main/java/web/model/Cars.java
@@ -1,4 +1,14 @@
 package web.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="cars")
 public class Cars {
+    @Id
+    @GeneratedValue()
+    private Long id;
+    private String model;
+    private String color;
+
 }
